4cef1d5e0beeac4223dd2c9c38153f0276a93296
Dylan <dylan.calle.dev@gmail.com>
2026-03-23 09:25:09 -0400

    build: add toast from sonner

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button";
-
+import { toast } from "sonner";
 function App() {
   return (
     <div className="flex-col w-full h-screen">
       <div className="flex flex-col justify-center items-center w-full h-full gap-2">
-        <Button size={`lg`}>Testing button</Button>
+        <Button size={`lg`} onClick={() => toast.success("Testing button")}>
+          Testing button
+        </Button>
         <Button variant="secondary">Testing button</Button>
         <Button variant="outline">Testing button</Button>
         <Button variant="destructive">Testing button</Button>
