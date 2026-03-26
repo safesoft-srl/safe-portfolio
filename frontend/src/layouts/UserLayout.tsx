a0694c12cbb1d3dc35d6ced65e2890666fed97c5
Dylan <dylan.calle.dev@gmail.com>
2026-03-26 14:52:31 -0400

    feat: add modetoggle component in user layout

--- frontend/src/layouts/UserLayout.tsx
+++ frontend/src/layouts/UserLayout.tsx
@@ -1,4 +1,5 @@
 import { AppSidebar } from "@/components/app-sidebar";
+import { ModeToggle } from "@/components/ModeToggle";
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -16,7 +17,7 @@ export default function UserLayout({ children }: { children?: React.ReactNode })
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+        <header className="flex h-16 shrink-0 items-center justify-between gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
           <div className="flex items-center gap-2 px-4">
             <SidebarTrigger className="-ml-1" />
             <Separator orientation="vertical" className="mr-2 data-[orientation=vertical]:h-4" />
@@ -31,6 +32,9 @@ export default function UserLayout({ children }: { children?: React.ReactNode })
                 </BreadcrumbItem>
               </BreadcrumbList>
             </Breadcrumb>
+          </div>
+          <div className="mr-5">
+            <ModeToggle />
           </div>
         </header>
 
