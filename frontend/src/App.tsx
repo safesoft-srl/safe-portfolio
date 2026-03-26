d46f8f1358b722531edf9e437af4b762b21e6690
Dylan <dylan.calle.dev@gmail.com>
2026-03-26 14:52:04 -0400

    feat: add support for dark and light mode

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,12 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
+import { ThemeProvider } from "./components/providers/ThemeProvider";
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  );
 }
 
 export default App;
