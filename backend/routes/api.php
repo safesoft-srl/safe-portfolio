9c7676173a78b57c5c6df9cb36ce3ca8b934f5fe
mr_bryanvm <bryan.vasquez.456@gmail.com>
2026-03-29 02:44:03 -0400

    feat: configure auth api routes for login

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+use App\Http\Controllers\AuthController;
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function ($router) {
+    Route::post('/login', [AuthController::class, 'login']);
 });
