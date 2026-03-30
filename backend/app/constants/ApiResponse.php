203a3f186676a78650ce6fc1f0e49b223066dfc9
mejia-87 <mejiasanchez3010@gmail.com>
2026-03-30 16:59:43 -0400

    feature: Add classes to the standardized API responses

---
+++ backend/app/constants/ApiResponse.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Constants;
+
+class ApiResponse
+{
+    public static function success($data = null, string $message = '', int $status = 200)
+    {
+        return response()->json([
+            'success' => true,
+            'data' => $data,
+            'message' => $message
+        ], $status);
+    }
+
+    public static function error(string $message = '', int $status = 400)
+    {
+        return response()->json([
+            'success' => false,
+            'data' => null,
+            'message' => $message
+        ], $status);
+    }
+}
