b88b7b7f9aa9ca385f5a047c643ee40a092c34d0
Dylan <dylan.calle.dev@gmail.com>
2026-03-29 23:52:45 -0400

    feat: add guarding routes

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import Login from "./pages/Login";
 import UserLayout from "./layouts/UserLayout";
+import { ProtectedRoute } from "./components/auth/ProtectedRoute";
+import { GuestRoute } from "./components/auth/GuestRoute";
 
 export const router = createBrowserRouter([
   {
@@ -9,23 +11,41 @@ export const router = createBrowserRouter([
   },
   {
     path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/register",
-    element: <div>Register Page</div>,
-  },
-  {
-    path: "/dashboard",
-    element: <UserLayout />,
+    element: <GuestRoute />,
     children: [
       {
         path: "",
-        element: <div>Dashboard Page</div>,
+        element: <Login />,
       },
+    ],
+  },
+  {
+    path: "/register",
+    element: <GuestRoute />,
+    children: [
       {
-        path: "test",
-        element: <div>Testing Page</div>,
+        path: "",
+        element: <div>Register Page</div>,
+      },
+    ],
+  },
+  {
+    path: "/dashboard",
+    element: <ProtectedRoute />,
+    children: [
+      {
+        path: "",
+        element: <UserLayout />,
+        children: [
+          {
+            path: "",
+            element: <div>Dashboard Page</div>,
+          },
+          {
+            path: "test",
+            element: <div>Testing Page</div>,
+          },
+        ],
       },
     ],
   },
