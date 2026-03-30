944c9834db20926032fd7521e9324bf6dd14a4f6
mr_bryanvm <bryan.vasquez.456@gmail.com>
2026-03-30 08:33:56 -0400

    fix: make sslmode dynamic to prevent GitHub Actions CI/CD failures on local dbs

--- backend/config/database.php
+++ backend/config/database.php
@@ -75,7 +75,7 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
-            'sslmode' => 'require', // Se queda Strict SSL para Neon
+            'sslmode' => env('DB_SSLMODE', 'prefer'),
         ],
 
         'sqlsrv' => [
