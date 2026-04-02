f58542d66bb9d666d4d67b2c056e993d96f3eec5
Adrian Lizarazu <adrianlizarazu64@gmail.com>
2026-04-01 21:11:46 -0400

    UserData

--- frontend/src/pages/UserAccount.tsx
+++ frontend/src/pages/UserAccount.tsx
@@ -1,7 +1,43 @@
+
+interface SectionProps {
+  Datos: string;
+  Biografia: string;
+  Usuario: string;
+  Descripcion: string;
+  imagen?: string;
+
+}
+
+function Section({ Datos, Biografia, Usuario, Descripcion,imagen }: SectionProps) {
+  return (
+    <div>
+      <h2>{Datos}</h2>
+      <h2>{Biografia}</h2>
+      <p>{Usuario}</p>
+      <p>{Descripcion}</p>
+      {/* Imagen */}
+      {imagen && (
+        <img
+          src={imagen}
+          alt="Imagen de usuario"
+          width="150"
+        />
+      )}
+    </div>
+  );
+}
+
 export default function UserAccount() {
   return (
     <div>
-      <h1>User Account</h1>
+      <Section
+        Datos="Datos del Usuario"
+        Biografia="Biografía del Usuario"
+        Usuario="nombre de usuario: "
+        Descripcion="Descripción del Usuario: "
+        imagen="https://via.placeholder.com/150"
+
+      />
     </div>
   );
 }
