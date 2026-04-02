d44c335c8f0aafdf2582958a04f5ae12c34916b8
Hedi <montanohedilberto@gmail.com>
2026-04-02 00:23:19 -0400

    feat: Add routes dashboard & profile

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Login from "./pages/Login";
+import Profile from "./pages/Profile";
 import UserLayout from "./layouts/UserLayout";
 
 export const router = createBrowserRouter([
@@ -26,6 +27,30 @@ export const router = createBrowserRouter([
       {
         path: "test",
         element: <div>Testing Page</div>,
+      },
+      {
+        path: "profile",
+        element: <Profile />,
+      },
+      {
+        path: "projects",
+        element: <div>Projects Page</div>,
+      },
+      {
+        path: "skills",
+        element: <div>Skills Page</div>,
+      },
+      {
+        path: "experience",
+        element: <div>Experience Page</div>,
+      },
+      {
+        path: "configuration",
+        element: <div>Configuration Page</div>,
+      },
+      {
+        path: "reports",
+        element: <div>Reports Page</div>,
       },
     ],
   },
