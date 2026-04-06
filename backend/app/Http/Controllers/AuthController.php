d60eb34d8e4395bd91ae63d48ee8481151bd4666
mr_bryanvm <bryan.vasquez.456@gmail.com>
2026-04-05 22:41:23 -0400

    style: apply Pint formatting to AuthController

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -23,7 +23,7 @@ class AuthController extends Controller
 
         $credentials = [
             $fieldType => $loginValue,
-            'password' => $password
+            'password' => $password,
         ];
 
         try {
