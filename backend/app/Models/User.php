fc11428145ea25fb4e601c788d998559562d1f8a
mejia-87 <mejiasanchez3010@gmail.com>
2026-04-20 18:46:36 -0400

    refactor: WorkExperience model associate with Portfolio instead of User

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -84,9 +84,4 @@ class User extends Authenticatable implements JWTSubject
     {
         return $this->hasMany(Portfolio::class);
     }
-
-    public function workExperiences()
-    {
-        return $this->hasMany(WorkExperience::class);
-    }
 }
