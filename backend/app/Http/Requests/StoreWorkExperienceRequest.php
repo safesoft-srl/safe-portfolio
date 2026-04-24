8f3b46bd67959b0b906705b2c2004f4c8cefa1e7
mr_bryanvm <bryan.vasquez.456@gmail.com>
2026-04-24 19:48:41 -0400

    feat: change maximum character quantity for company and position

--- backend/app/Http/Requests/StoreWorkExperienceRequest.php
+++ backend/app/Http/Requests/StoreWorkExperienceRequest.php
@@ -22,8 +22,8 @@ class StoreWorkExperienceRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'company' => 'required|string|max:255',
-            'position' => 'required|string|max:255',
+            'company' => 'required|string|max:100',
+            'position' => 'required|string|max:100',
             'start_date' => 'required|date',
             'end_date' => 'nullable|date|after_or_equal:start_date',
             'is_current' => 'boolean',
