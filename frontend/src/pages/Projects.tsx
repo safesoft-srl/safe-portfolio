5b7d0e383338684e4db68be5331e268b05c03c53
Hedi <montanohedilberto@gmail.com>
2026-04-28 03:45:03 -0400

    fix: delete variable no used

--- frontend/src/pages/Projects.tsx
+++ frontend/src/pages/Projects.tsx
@@ -22,7 +22,6 @@ import {
   AlertDialogAction,
 } from "@/components/ui/alert-dialog";
 import { showErrorToast } from "@/components/ui/showErrorToast";
-import { SkillComboBox } from "@/components/SkillComboBox";
 
 type Project = {
   id: number;
