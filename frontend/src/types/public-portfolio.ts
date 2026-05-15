8972f55ba252a4cfde6c0107dc0404b99288ce53
Hedi <montanohedilberto@gmail.com>
2026-05-14 23:21:23 -0400

    feat: Add AcademicFormation component and update PublicPortfolio to display academic formation

--- frontend/src/types/public-portfolio.ts
+++ frontend/src/types/public-portfolio.ts
@@ -24,6 +24,21 @@ export type WorkExperience = {
   updated_at: Date;
 };
 
+export type AcademicTraining = {
+  id: number;
+  portfolio_id: number;
+  institution_name: string;
+  title: string;
+  field_of_study: string;
+  start_date: string;
+  end_date: string | null;
+  is_current: boolean;
+  description: string;
+  is_visible: boolean;
+  created_at: Date;
+  updated_at: Date;
+};
+
 export type ProfileData = {
   id: number;
   user_id: number;
@@ -40,6 +55,7 @@ export type ProfileData = {
   updated_at: Date;
   portfolio_skills: PortfolioSkill[];
   work_experiences: WorkExperience[];
+  academyc_trainings: AcademicTraining[];
   projects: Project[];
 };
 export type ApiProfilePayload = {
