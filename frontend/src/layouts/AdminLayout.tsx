885b0d2228d7da809bbe62f47e6fa8d2f98508c5
Dylan Calle <dylan.calle.dev@gmail.com>
2026-05-27 19:43:09 -0400

    feat: add basic components for admin sidebar

---
+++ frontend/src/layouts/AdminLayout.tsx
@@ -0,0 +1,33 @@
+import { AppSidebar } from "@/components/app-sidebar";
+import { ModeToggle } from "@/components/ModeToggle";
+import { Breadcrumb, BreadcrumbList } from "@/components/ui/breadcrumb";
+import { Separator } from "@/components/ui/separator";
+import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { Outlet } from "react-router-dom";
+
+export default function AdminLayout({ children }: { children?: React.ReactNode }) {
+  return (
+    <SidebarProvider>
+      <AppSidebar admin={true} />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center justify-between gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator orientation="vertical" className="mr-2 data-[orientation=vertical]:h-4" />
+            <Breadcrumb>
+              <BreadcrumbList>
+                {/* Breadcrumb items dinámicos irán aquí más adelante */}
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
+          <div className="mr-5">
+            <ModeToggle />
+          </div>
+        </header>
+
+        {/* FOR THE FRONTEND TEAM: Here goes the content of the page */}
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">{children || <Outlet />}</div>
+      </SidebarInset>
+    </SidebarProvider>
+  );
+}
