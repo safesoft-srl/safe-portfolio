b4e4cc58257306516c448cbc55e58d306864c0dc
Dylan Calle <dylan.calle.dev@gmail.com>
2026-06-15 23:19:53 -0400

    feat: hide toggle button

--- frontend/src/components/ModeToggle.tsx
+++ frontend/src/components/ModeToggle.tsx
@@ -15,7 +15,7 @@ export function ModeToggle() {
     <DropdownMenu>
       <DropdownMenuTrigger
         render={
-          <Button variant="outline" size="icon">
+          <Button className="dark:hidden hidden" variant="outline" size="icon">
             <SunIcon className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
             <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
             <span className="sr-only">Cambiar tema</span>
